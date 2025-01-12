3a3a55c99cd3b8cefde2f5a092d0bcf4cdfa39fc
ganu <gwbaik9717@icloud.com>
2025-01-12 18:07:52 +0900

    refactor: utils/dom 내 불필요한 함수 제거

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -3,20 +3,7 @@ export const $ = <T extends HTMLElement = HTMLElement>(
   parent = document
 ) => parent.querySelector<T>(selector);
 
-export const $all = (selector: string, parent = document) => [
-  ...parent.querySelectorAll<HTMLElement>(selector),
-];
-
 export const createElement = (tagName: string) => {
   const element = document.createElement(tagName);
   return element;
-};
-
-export const removeElements = (parent: HTMLElement, selectors: string) => {
-  const filtered = Array.from(parent.children).filter((child) => {
-    return !child.matches(selectors);
-  });
-
-  parent.innerHTML = "";
-  parent.append(...filtered);
 };
